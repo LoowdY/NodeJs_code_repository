4e7b445baef6ee368c7cac05a6e03845c3df7efe
João Renan <102002118+LoowdY@users.noreply.github.com>
2022-09-08 00:08:45 -0300

    Add files via upload

---
+++ template_string.js
@@ -0,0 +1,16 @@
+//template string
+
+
+// In JavaScript, we can concatenate string just like whe can sum nubers. take a look at the example:
+
+const nome = "Jorge";
+const nomeCompleto = nome + " Petrucchio";
+
+console.log(nomeCompleto);
+
+//There is other way to do this. Take a brief look at the exmaple below:
+
+const nomeCompleto_Different = `Meu nome é ${nome}`;
+console.log(nomeCompleto_Different);
+
+//Notice that I not using " but ` 
